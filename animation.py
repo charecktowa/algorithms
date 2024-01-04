06706cecbc1c4f33ceeeacf2c8138a9a30e1d3d6
Andrea Gutiérrez Alfaro <charecktowa@protonmail.com>
2024-01-04 10:18:25 -0600

    Added while loop for population

--- animation.py
+++ animation.py
@@ -1,3 +1,4 @@
+from hmac import new
 import os
 import numpy as np
 
@@ -77,17 +78,228 @@ class GeneticAlgorithm(Scene):
         # Animation to show the population
 
         for _ in range(generations):
-            # Here we draw the squares corresponding to the population
+            new_population = []
+
+            while len(new_population) < population_size:
+                # Here we draw the squares corresponding to the population
+                population_squares = VGroup(
+                    *[
+                        VGroup(
+                            *[Square(side_length=0.8) for _ in range(chromosome_length)]
+                        ).arrange(buff=0.5)
+                        for _ in range(population_size)
+                    ]
+                ).arrange(DOWN * 0.38)
+
+                for i, row in enumerate(population_squares):
+                    for j, square in enumerate(row):
+                        number = Text(str(population[i][j]))
+                        number.move_to(square)
+                        population_squares[i][j].add(number)
+
+                self.play(FadeIn(population_squares))
+                self.wait()
+
+                # Now we calculate the fitness of each individual
+                fitness = fitness_function(population)
+                fitness_labels = [Text("Fitness: " + str(fit)) for fit in fitness]
+                for group, label in zip(population_squares, fitness_labels):
+                    label.next_to(group, RIGHT)
+                    self.play(Write(label))
+                self.wait()
+
+                self.play(
+                    FadeOut(population_squares),
+                    *[FadeOut(label) for label in fitness_labels],
+                )
+
+                # Select the parents for reproduction
+                parent1, parent2, tournament = selection(population, tournament_size)
+
+                # Show who are going to compete
+                tournament_squares = VGroup(
+                    *[
+                        VGroup(
+                            *[Square(side_length=0.8) for _ in range(chromosome_length)]
+                        ).arrange(buff=0.5)
+                        for _ in range(tournament_size)
+                    ]
+                ).arrange(DOWN)
+
+                for i, row in enumerate(tournament_squares):
+                    for j, square in enumerate(row):
+                        number = Text(str(tournament[i][j]))
+                        number.move_to(square)
+                        tournament_squares[i][j].add(number)
+
+                self.play(FadeIn(tournament_squares))
+                self.wait()
+
+                # Indicate the parents TODO: if it repeats dont show it again
+                for i, individual in enumerate(tournament):
+                    if np.array_equal(individual, parent1) or np.array_equal(
+                        individual, parent2
+                    ):
+                        self.play(Indicate(tournament_squares[i]))
+                        self.wait()
+
+                self.play(FadeOut(tournament_squares))
+
+                # Show the parents
+                parent_squares = VGroup(
+                    *[
+                        VGroup(*[Square() for _ in range(chromosome_length)]).arrange(
+                            buff=0.5
+                        )
+                        for _ in range(2)  # Since there are only two parents
+                    ]
+                ).arrange(DOWN)
+
+                parents = [parent1, parent2]
+                for i, row in enumerate(parent_squares):
+                    for j, square in enumerate(row):
+                        number = Text(str(parents[i][j]))
+                        number.move_to(square)
+                        parent_squares[i][j].add(number)
+
+                self.play(FadeIn(parent_squares))
+                self.wait()
+
+                # Perform crossover
+                crossover_point = rng.integers(1, chromosome_length)
+                child1 = np.concatenate(
+                    (parent1[:crossover_point], parent2[crossover_point:])
+                )
+                child2 = np.concatenate(
+                    (parent2[:crossover_point], parent1[crossover_point:])
+                )
+
+                # Animate crossover line
+                crossover_line = Line(
+                    start=parent_squares[0][crossover_point - 1].get_corner(UP + RIGHT),
+                    end=parent_squares[1][crossover_point - 1].get_corner(DOWN + RIGHT),
+                    color=RED,
+                    stroke_width=11,
+                )
+                crossover_line.shift(RIGHT * 0.2)
+                self.play(Create(crossover_line))
+                self.wait()
+
+                # Apply color changes to squares in parent_squares
+                self.play(
+                    *[
+                        square.animate.set_color(RED)
+                        for square in parent_squares[0][crossover_point:]
+                    ]
+                    + [
+                        square.animate.set_color(RED)
+                        for square in parent_squares[1][:crossover_point]
+                    ]
+                )
+                self.wait()
+
+                self.play(FadeOut(crossover_line))
+
+                # Shrink and move the parent_squares
+                self.play(parent_squares.animate.scale(0.35).to_edge(UP, buff=1))
+                self.wait()
+
+                # Animate the offspring
+                offspring_squares = VGroup(
+                    *[
+                        VGroup(*[Square() for _ in range(chromosome_length)]).arrange(
+                            buff=0.5
+                        )
+                        for _ in range(2)  # Since there are only two offsprings
+                    ]
+                ).arrange(DOWN)
+
+                offspring = [child1, child2]
+                for i, row in enumerate(offspring_squares):
+                    for j, square in enumerate(row):
+                        number = Text(str(offspring[i][j]))
+                        number.move_to(square)
+                        offspring_squares[i][j].add(number)
+
+                offspring_squares.scale(0.8).to_edge(DOWN, buff=1)
+                self.play(FadeIn(offspring_squares))
+                self.wait()
+
+                # Apply color changes to squares in offspring_squares
+                self.play(
+                    *[
+                        square.animate.set_color(BLUE)
+                        for square in offspring_squares[0][crossover_point:]
+                    ]
+                    + [
+                        square.animate.set_color(BLUE)
+                        for square in offspring_squares[1][:crossover_point]
+                    ]
+                )
+                self.wait()
+
+                # Apply color changes to squares in offspring_squares
+                self.play(
+                    *[
+                        square.animate.set_color(WHITE)
+                        for square in offspring_squares[0][crossover_point:]
+                    ]
+                    + [
+                        square.animate.set_color(WHITE)
+                        for square in offspring_squares[1][:crossover_point]
+                    ]
+                )
+
+                child1, mutated1 = mutation(child1, mutation_rate)
+                child2, mutated2 = mutation(child2, mutation_rate)
+
+                if mutated1 or mutated2:
+                    offspring = [child1, child2]
+                    self.play(
+                        *[
+                            Indicate(offspring_squares[0][index], color=RED)
+                            for index in mutated1
+                        ]
+                        + [
+                            Indicate(offspring_squares[1][index], color=RED)
+                            for index in mutated2
+                        ]
+                    )
+
+                    for i, mutated_indices in enumerate([mutated1, mutated2]):
+                        for index in mutated_indices:
+                            number = Text(str(offspring[i][index]))
+                            offspring_squares[i][index].remove(
+                                offspring_squares[i][index][1]
+                            )
+                            number.move_to(offspring_squares[i][index])
+                            offspring_squares[i][index].add(number)
+                    self.wait()
+
+                # Add the offspring to the new population
+                new_population.append(child1)
+                new_population.append(child2)
+
+                # Fade out the offspring
+                self.play(FadeOut(offspring_squares))
+                self.play(*[FadeOut(mob) for mob in self.mobjects])
+                self.wait()
+
+            # Update the population
+            new_population = np.array(new_population)
+            population = new_population
+
+            # Show final population from this generation
+            print("printing...")
             population_squares = VGroup(
                 *[
-                    VGroup(
-                        *[Square(side_length=0.8) for _ in range(chromosome_length)]
-                    ).arrange(buff=0.5)
+                    VGroup(*[Square() for _ in range(chromosome_length)]).arrange(
+                        buff=0.5
+                    )
                     for _ in range(population_size)
                 ]
             ).arrange(DOWN * 0.38)
 
-            # Here we generate random numbers for each square
             for i, row in enumerate(population_squares):
                 for j, square in enumerate(row):
                     number = Text(str(population[i][j]))
@@ -96,188 +308,6 @@ class GeneticAlgorithm(Scene):
 
             self.play(FadeIn(population_squares))
             self.wait()
-
-            # Now we calculate the fitness of each individual
-            fitness = fitness_function(population)
-            fitness_labels = [Text("Fitness: " + str(fit)) for fit in fitness]
-            for group, label in zip(population_squares, fitness_labels):
-                label.next_to(group, RIGHT)
-                self.play(Write(label))
-            self.wait()
-
-            self.play(
-                FadeOut(population_squares),
-                *[FadeOut(label) for label in fitness_labels],
-            )
-
-            # Select the parents for reproduction
-            parent1, parent2, tournament = selection(population, tournament_size)
-
-            # Show who are going to compete
-            tournament_squares = VGroup(
-                *[
-                    VGroup(
-                        *[Square(side_length=0.8) for _ in range(chromosome_length)]
-                    ).arrange(buff=0.5)
-                    for _ in range(tournament_size)
-                ]
-            ).arrange(DOWN)
-
-            for i, row in enumerate(tournament_squares):
-                for j, square in enumerate(row):
-                    number = Text(str(tournament[i][j]))
-                    number.move_to(square)
-                    tournament_squares[i][j].add(number)
-
-            self.play(FadeIn(tournament_squares))
-            self.wait()
-
-            # Indicate the parents TODO: if it repeats dont show it again
-            for i, individual in enumerate(tournament):
-                if np.array_equal(individual, parent1) or np.array_equal(
-                    individual, parent2
-                ):
-                    self.play(Indicate(tournament_squares[i]))
-                    self.wait()
-
-            self.play(FadeOut(tournament_squares))
-
-            # Show the parents
-            parent_squares = VGroup(
-                *[
-                    VGroup(*[Square() for _ in range(chromosome_length)]).arrange(
-                        buff=0.5
-                    )
-                    for _ in range(2)  # Since there are only two parents
-                ]
-            ).arrange(DOWN)
-
-            parents = [parent1, parent2]
-            for i, row in enumerate(parent_squares):
-                for j, square in enumerate(row):
-                    number = Text(str(parents[i][j]))
-                    number.move_to(square)
-                    parent_squares[i][j].add(number)
-
-            self.play(FadeIn(parent_squares))
-            self.wait()
-
-            # Perform crossover
-            crossover_point = rng.integers(1, chromosome_length)
-            child1 = np.concatenate(
-                (parent1[:crossover_point], parent2[crossover_point:])
-            )
-            child2 = np.concatenate(
-                (parent2[:crossover_point], parent1[crossover_point:])
-            )
-
-            # Animate crossover line
-            crossover_line = Line(
-                start=parent_squares[0][crossover_point - 1].get_corner(UP + RIGHT),
-                end=parent_squares[1][crossover_point - 1].get_corner(DOWN + RIGHT),
-                color=RED,
-                stroke_width=11,
-            )
-            crossover_line.shift(RIGHT * 0.2)
-            self.play(Create(crossover_line))
-            self.wait()
-
-            # Apply color changes to squares in parent_squares
-            self.play(
-                *[
-                    square.animate.set_color(RED)
-                    for square in parent_squares[0][crossover_point:]
-                ]
-                + [
-                    square.animate.set_color(RED)
-                    for square in parent_squares[1][:crossover_point]
-                ]
-            )
-            self.wait()
-
-            self.play(FadeOut(crossover_line))
-
-            # Shrink and move the parent_squares
-            self.play(parent_squares.animate.scale(0.35).to_edge(UP, buff=1))
-            self.wait()
-
-            # Animate the offspring
-            offspring_squares = VGroup(
-                *[
-                    VGroup(*[Square() for _ in range(chromosome_length)]).arrange(
-                        buff=0.5
-                    )
-                    for _ in range(2)  # Since there are only two offsprings
-                ]
-            ).arrange(DOWN)
-
-            offspring = [child1, child2]
-            for i, row in enumerate(offspring_squares):
-                for j, square in enumerate(row):
-                    number = Text(str(offspring[i][j]))
-                    number.move_to(square)
-                    offspring_squares[i][j].add(number)
-
-            offspring_squares.scale(0.8).to_edge(DOWN, buff=1)
-            self.play(FadeIn(offspring_squares))
-            self.wait()
-
-            # Apply color changes to squares in offspring_squares
-            self.play(
-                *[
-                    square.animate.set_color(BLUE)
-                    for square in offspring_squares[0][crossover_point:]
-                ]
-                + [
-                    square.animate.set_color(BLUE)
-                    for square in offspring_squares[1][:crossover_point]
-                ]
-            )
-            self.wait()
-
-            # Apply color changes to squares in offspring_squares
-            self.play(
-                *[
-                    square.animate.set_color(WHITE)
-                    for square in offspring_squares[0][crossover_point:]
-                ]
-                + [
-                    square.animate.set_color(WHITE)
-                    for square in offspring_squares[1][:crossover_point]
-                ]
-            )
-
-            print("Before mutation:")
-            print(child1)
-            print(child2)
-            child1, mutated1 = mutation(child1, mutation_rate)
-            child2, mutated2 = mutation(child2, mutation_rate)
-            print("After mutation:")
-            print(child1, mutated1)
-            print(child2, mutated2)
-
-            if mutated1 or mutated2:
-                offspring = [child1, child2]
-                self.play(
-                    *[
-                        Indicate(offspring_squares[0][index], color=RED)
-                        for index in mutated1
-                    ]
-                    + [
-                        Indicate(offspring_squares[1][index], color=RED)
-                        for index in mutated2
-                    ]
-                )
-
-                for i, mutated_indices in enumerate([mutated1, mutated2]):
-                    for index in mutated_indices:
-                        number = Text(str(offspring[i][index]))
-                        offspring_squares[i][index].remove(
-                            offspring_squares[i][index][1]
-                        )
-                        number.move_to(offspring_squares[i][index])
-                        offspring_squares[i][index].add(number)
-                self.wait()
 
 
 if __name__ == "__main__":
